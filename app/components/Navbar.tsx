e11983d0d20f168181d73b1eed6114ea47ab646f
yohrdy243 <yohrdy243@gmail.com>
2023-06-21 14:49:04 -0500

    first commit

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav className="px-20 py-2 text-white bg-stone-800 flex justify-between items-center">
+      <div>
+        <h1 className="text-2xl">LOGO</h1>
+        <h2>ALbujar Rafaile & Asociados</h2>
+      </div>
+      <ul className="flex text-sm uppercase gap-4 cursor-pointer">
+        <li>inicio</li>
+        <li>quienes somos</li>
+        <li>clientes</li>
+        <li>Contacto</li>
+      </ul>
+    </nav>
+  );
+}
