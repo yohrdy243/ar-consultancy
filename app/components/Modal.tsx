00aa081ece53105e6798e137344418d627924711
Yohrdy Guevara Lescano <yohrdy243@gmail.com>
2024-08-07 01:19:48 -0500

    Se agrega la información a en modals

--- app/components/Modal.tsx
+++ app/components/Modal.tsx
@@ -2,68 +2,70 @@ import { ReactNode, useEffect, useRef } from "react";
 import { AiOutlineClose } from "react-icons/ai";
 import Swal from "sweetalert2";
 
+export type TModalProps = {
+    children?: ReactNode;
+    isOpen: boolean;
+    handleClose: () => void;
+    title?: string;
+};
+
 export default function Modal({
     children,
     isOpen,
     handleClose,
     title,
-  }: {
-    children: ReactNode;
-    isOpen: boolean;
-    handleClose: () => void;
-    title: string;
-  }) {
+}: TModalProps) {
     const modalRef = useRef<HTMLDivElement>(null);
-  
+
     useEffect(() => {
-      const handleOutsideClick = (event: MouseEvent) => {
-        if (
-          modalRef.current &&
-          !modalRef.current.contains(event.target as Node) &&
-          !Swal.isVisible() // Check if Swal is visible
-        ) {
-          handleClose();
+        const handleOutsideClick = (event: MouseEvent) => {
+            if (
+                modalRef.current &&
+                !modalRef.current.contains(event.target as Node) &&
+                !Swal.isVisible() // Check if Swal is visible
+            ) {
+                handleClose();
+            }
+        };
+
+        if (isOpen) {
+            document.body.style.overflow = "hidden";
+            window.addEventListener("mousedown", handleOutsideClick);
+        } else {
+            document.body.style.overflow = "";
+            window.removeEventListener("mousedown", handleOutsideClick);
         }
-      };
-  
-      if (isOpen) {
-        document.body.style.overflow = "hidden";
-        window.addEventListener("mousedown", handleOutsideClick);
-      } else {
-        document.body.style.overflow = "";
-        window.removeEventListener("mousedown", handleOutsideClick);
-      }
-  
-      return () => {
-        window.removeEventListener("mousedown", handleOutsideClick);
-      };
+
+        return () => {
+            window.removeEventListener("mousedown", handleOutsideClick);
+        };
     }, [isOpen, handleClose]);
-  
+
     if (!isOpen) return null;
-  
+
     return (
-      <div className="fixed inset-0 z-50 flex justify-center items-center">
-        <div
-          className="fixed inset-0 bg-black bg-opacity-50"
-          onClick={handleClose}
-        ></div>
-        <div className="fixed left-0 right-0 top-0 bottom-0 flex justify-center items-center">
-          <div
-            ref={modalRef}
-            className="p-6 bg-white min-w-[80%] lg:min-w-[60%] rounded-md max-w-screen-md overflow-y-auto"
-          >
-            <div className="flex justify-between mb-8">
-              <h1 className="font-bold text-xl">{title}</h1>
-              <div
-                className="hover:border rounded-md p-1 text-gray-400"
+        <div className="fixed inset-0 z-50 flex justify-center items-center">
+            <div
+                className="fixed inset-0 bg-black bg-opacity-50"
                 onClick={handleClose}
-              >
-                <AiOutlineClose />
-              </div>
+            ></div>
+            <div className="fixed left-0 right-0 top-0 bottom-0 flex justify-center items-center">
+                <div
+                    ref={modalRef}
+                    className="p-6 bg-white min-w-[80%] lg:min-w-[60%] rounded-md max-w-screen-md overflow-y-auto"
+                >
+                    <div className="flex justify-between mb-8">
+                        <h1 className="font-bold text-xl">{title}</h1>
+                        <div
+                            className="hover:border rounded-md p-1 text-gray-400"
+                            onClick={handleClose}
+                        >
+                            <AiOutlineClose />
+                        </div>
+                    </div>
+                    {children}
+                </div>
             </div>
-            {children}
-          </div>
         </div>
-      </div>
     );
-  }
+}
--- app/nuestroequipo/page.tsx
+++ app/nuestroequipo/page.tsx
@@ -7,6 +7,9 @@ import PageHeader from "../components/PageHeader";
 import { HiOutlineMail } from "react-icons/hi";
 import { useModal } from "../hooks/useModal";
 import { ModalFormContact } from "../components/FormContact";
+import Modal, { TModalProps } from "../components/Modal";
+import { FaBriefcase } from "react-icons/fa";
+import { useState } from "react";
 
 type TExperience = {
     title: string;
@@ -120,22 +123,108 @@ const teamData: TTeamData[] = [
     },
 ];
 
-function TimeLine({ data }: { data: TExperience[] }) {
+function Personal({
+    imageSrc,
+    name,
+    experiences,
+    certifications,
+    modal: { handleClose, isOpen },
+}: {
+    imageSrc: string;
+    name: string;
+    experiences: TExperience[];
+    certifications: TExperience[];
+    modal: TModalProps;
+}) {
+    const [step, setStep] = useState(0);
     return (
-        <ol className="relative text-gray-500 border-l border-gray-200 cursor-pointer hover:text-green-700">
-            {data.map(({ title, subTitle, description }) => (
-                <li className="mb-10 ml-6" key={`EXPERINCE_${title}`}>
-                    <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white ">
-                        <BsCalendarCheckFill />
-                    </span>
-                    <h3 className="font-medium  leading-tight">{title}</h3>
-                    {subTitle ? <p className="text-sm">{subTitle} </p> : null}
-                    {description ? (
-                        <p className="text-xs   ">{description}</p>
-                    ) : null}
-                </li>
+        <Modal
+            isOpen={isOpen}
+            handleClose={handleClose}
+            title="Perfil Profesional"
+        >
+            <div className="flex flex-row gap-4 w-full">
+                <div className="flex flex-col p-4">
+                    <Image
+                        className="rounded-t-xl"
+                        src={imageSrc}
+                        alt={name}
+                        width={400}
+                        height={400}
+                    />
+                    <div className="flex flex-col gap-1 justify-center items-center pt-2">
+                        <h1 className="text-2xl font-semibold">{name}</h1>
+                    </div>
+                    <div className="flex gap-2 mt-2">
+                        <div
+                            className="flex gap-2 border rounded-md px-2 py-1 w-fit hover:text-[#ff6915]"
+                            onClick={() => setStep(0)}
+                        >
+                            <span>Experiencia</span>
+                        </div>
+                        <div
+                            className="flex gap-2 items-center border rounded-md px-2 py-1 w-fit hover:text-[#ff6915]"
+                            onClick={() => setStep(1)}
+                        >
+                            <span>Certificaciones</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {step === 0 ? (
+                        <>
+                            <h1 className="text-xl py-2 font-bold">Experiencia</h1>
+                            <TimeLine data={experiences} />
+                        </>
+                    ) : (
+                        <>
+                            <h1 className="text-xl py-2 font-bold">Certificaciones</h1>
+                            <TimeLine data={certifications} />
+                        </>
+                    )}
+                </div>
+            </div>
+        </Modal>
+    );
+}
+
+function TimeLine({ data }: { data: TExperience[] }) {
+    const chunkSize = 4;
+    const chunks = [];
+
+    for (let i = 0; i < data.length; i += chunkSize) {
+        chunks.push(data.slice(i, i + chunkSize));
+    }
+
+    return (
+        <div className="grid grid-cols-2 gap-4 w-full">
+            {chunks.map((chunk, chunkIndex) => (
+                <ol
+                    key={`TIMELINE_CHUNK_${chunkIndex}`}
+                    className="relative text-gray-500 border-l border-gray-200 cursor-pointer hover:text-green-700"
+                >
+                    {chunk.map(({ title, subTitle, description }, index) => (
+                        <li
+                            className="mb-10 ml-6"
+                            key={`EXPERIENCE_${chunkIndex}_${index}`}
+                        >
+                            <span className="absolute flex items-center justify-center w-8 h-8 bg-gray-100 rounded-full -left-4 ring-4 ring-white">
+                                <BsCalendarCheckFill />
+                            </span>
+                            <h3 className="font-medium leading-tight">
+                                {title}
+                            </h3>
+                            {subTitle ? (
+                                <p className="text-sm">{subTitle}</p>
+                            ) : null}
+                            {description ? (
+                                <p className="text-xs">{description}</p>
+                            ) : null}
+                        </li>
+                    ))}
+                </ol>
             ))}
-        </ol>
+        </div>
     );
 }
 
@@ -148,6 +237,11 @@ function TeamCard({
     certifications,
 }: TTeamData) {
     const { show, handleShow, handleClose } = useModal();
+    const {
+        show: isOpen,
+        handleShow: handleShowPerson,
+        handleClose: handleClosePerson,
+    } = useModal();
 
     return (
         <>
@@ -155,30 +249,44 @@ function TeamCard({
                 title={`Contactar ${name}`}
                 {...{ show, handleClose }}
             />
-            <div className="px-8 col-span-1 flex flex-col items-center">
-                <div className="mb-20">
-                    <Image
-                        className="rounded-xl"
-                        src={imageSrc}
-                        alt={name}
-                        width={350}
-                        height={350}
-                    />
-                    <div>
-                        <h1 className="text-2xl font-semibold">{name}</h1>
-                        <p>{role}</p>
-                        <div className="text-sm text-gray-500 inline-flex items-center gap-3">
-                            {email}
-                            <div
-                                className="border rounded-md px-2 py-1 w-fit hover:text-green-500 hover:bg-green-100"
-                                onClick={handleShow}
-                            >
-                                <HiOutlineMail size={18} />
-                            </div>
+            <Personal
+                modal={{ isOpen, handleClose: handleClosePerson }}
+                imageSrc={imageSrc}
+                name={name}
+                certifications={certifications}
+                experiences={experience}
+            />
+            <div className="transition-all ease-in-out duration-400 col-span-1 flex flex-col items-center border mx-4 rounded-xl shadow-md hover:shadow-[#112c45] hover:scale-105 cursor-pointer">
+                <Image
+                    className="rounded-t-xl"
+                    src={imageSrc}
+                    alt={name}
+                    width={350}
+                    height={350}
+                />
+                <div className="flex flex-col gap-1 justify-center items-center p-4 mx-2">
+                    <h1 className="text-2xl font-semibold">{name}</h1>
+                    <p>{role}</p>
+                    <div className="text-sm text-[#112c45] items-center gap-3">
+                        {email}
+                    </div>
+                    <div className="flex gap-2 mt-2">
+                        <div
+                            className="flex gap-2 border rounded-md px-2 py-1 w-fit hover:text-[#ff6915]"
+                            onClick={handleShowPerson}
+                        >
+                            <span>Ver Perfil</span>
+                            <FaBriefcase size={18} />
+                        </div>
+                        <div
+                            className="flex gap-2 items-center border rounded-md px-2 py-1 w-fit hover:text-[#ff6915]"
+                            onClick={handleShow}
+                        >
+                            <span>Conactar</span>
+                            <HiOutlineMail size={20} />
                         </div>
                     </div>
                 </div>
-
             </div>
         </>
     );
@@ -187,9 +295,8 @@ function TeamCard({
 export default function page() {
     return (
         <section>
-            {" "}
             <PageHeader title="Nuestro Equipo" />
-            <div className="py-10 mt-20">
+            <div className="my-20">
                 <div className="lg:px-32 px-8">
                     <div className="flex justify-center">
                         <div className="grid grid-cols-1 lg:grid-cols-2 lg:divide-x gap-20 lg:gap-0">
